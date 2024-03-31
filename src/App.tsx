cd8f865a997baaff1c2e26f6fd4f99c3677c7cb7
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2024-03-31 13:14:32 +0200

    configure query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl'>this is app componen with working tailwind</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1 className='text-3xl'>this is app componen with working tailwind</h1>
+      </div>
+    </QueryClientProvider>
   );
 }
 
