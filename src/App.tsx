02590aabad664255996e7683883cbf6f4fca1c18
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2024-03-31 13:34:05 +0200

    install and configure mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const queryClient = new QueryClient();
 
