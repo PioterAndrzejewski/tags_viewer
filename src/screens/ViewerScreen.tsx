abc2c35d398c3be09b4d2f9ed3b7a5799e0972ee
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2024-03-31 14:31:14 +0200

    add table with mocked data

--- src/screens/ViewerScreen.tsx
+++ src/screens/ViewerScreen.tsx
@@ -1,7 +1,8 @@
+import { TagsViewer } from "src/components/TagsViewer/TagsViewer";
 import { Screen } from "src/components/common/Screen";
 
 export const ViewerScreen = () => (
   <Screen header='Tag Viewer'>
-    <h2>This is the viewer</h2>
+    <TagsViewer />
   </Screen>
 );
