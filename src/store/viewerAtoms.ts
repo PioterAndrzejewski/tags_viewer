5ed627724783755e3566c407ab840ca4d0a0866e
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2024-04-01 03:59:10 +0200

    remove unnecessary atoms

--- src/store/viewerAtoms.ts
+++ src/store/viewerAtoms.ts
@@ -1,11 +1,3 @@
-import { atom } from "jotai";
 import { atomWithLocation } from "jotai-location";
-
-import { Order, Sortable } from "src/services/tags";
-
-export const pageAtom = atom<number>(1);
-export const pageSizeAtom = atom<number>(10);
-export const orderAtom = atom<Order>("desc");
-export const sortableAtom = atom<Sortable>("popular");
 
 export const filtersAtom = atomWithLocation();
