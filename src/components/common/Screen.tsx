42720a43bcdf07c462e90c3f622af4dc72f2f1d9
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2024-04-01 12:21:42 +0200

    change padding on small device

--- src/components/common/Screen.tsx
+++ src/components/common/Screen.tsx
@@ -8,7 +8,7 @@ type ScreenProps = {
 export const Screen = (props: ScreenProps) => {
   const { header, children } = props;
   return (
-    <div className='flex flex-1 flex-col p-4'>
+    <div className='flex flex-1 flex-col p-2 lg:p-4'>
       <Header>{header}</Header>
       <main>{children}</main>
     </div>
