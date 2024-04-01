545b3f429099f445b10b92ea6c3e9a750d979693
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2024-04-01 11:57:09 +0200

    use api key, minor fixes

--- src/hooks/useTags.ts
+++ src/hooks/useTags.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { apiKey } from "src/services/apiConfig";
 import { queryKeys } from "src/services/queryKeys";
 import { getTags } from "src/services/tags";
 import { useFilters } from "./useFilters";
@@ -7,7 +8,13 @@ import { useFilters } from "./useFilters";
 export const useTags = () => {
   const { order, sort, itemsPerPage, pageNumber } = useFilters();
 
-  const params = { order, sort, page: pageNumber, pageSize: itemsPerPage };
+  const params = {
+    order,
+    sort,
+    page: pageNumber,
+    pageSize: itemsPerPage,
+    key: apiKey,
+  };
 
   return useQuery({
     queryKey: [...queryKeys.tags(params)],
--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,5 +1,8 @@
 const STACK_EXCHANGE_URL = "https://api.stackexchange.com/2.3";
 
+// no worries, I would never leave any api key or secret in codebase in commercial project ;)  
+export const apiKey = '*4MXipha7SDGJrtOWekKxA((';
+
 export const apiConfig = {
   get: {
     tags: `${STACK_EXCHANGE_URL}/tags`,
